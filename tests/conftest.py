ea836dcb86987b885227b7607110899270e22454
damianialex <alexdmail15@yahoo.com>
2020-03-22 01:02:08 -0400

    finished writing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,10 @@ conftest.py stores pytest fixtures that can be used by all test files.
 scope='session' --> all functions share one setup and teardown call
 scope='function' --> runs once per function
 '''
-
-from flask import Flask
-import json
 from bs4 import BeautifulSoup  # type: ignore
 import pytest  # type: ignore
 import os
 
-import utils
 import api
 
 
@@ -21,13 +17,16 @@ def client(request):
 
     return test_client
 
+
 @pytest.fixture(scope='session')
 def static_soup(client):
-	'''
-	HTML page is constantly updated with new informtion.
-	Cache static page to test functionality.
-	'''
-	html = open(os.path.join('tests', 'test_page.html'), 'r', encoding='utf-8').read()
-	soup = BeautifulSoup(html, 'html.parser')
+    '''
+    HTML page is constantly updated with new informtion.
+    Cache static page to test functionality.
+    '''
+    html = open(os.path.join('tests', 'test_page.html'),
+        'r',
+        encoding='utf-8').read()
+    soup = BeautifulSoup(html, 'html.parser')
 
-	return soup
+    return soup
