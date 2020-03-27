a70fe85c562eb0cdbfcf5a26f12e7ef7059362fe
damianialex <alexdmail15@yahoo.com>
2020-03-26 20:36:06 -0400

    removed unused Auth class

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,22 +30,3 @@ def static_soup(client):
     soup = BeautifulSoup(html, 'html.parser')
 
     return soup
-
-class AuthActions(object):
-    def __init__(self, client):
-        self._client = client
-        self.login()
-
-    def login(self, username='testuser@mail.com', password='pass123'):
-        password = app.bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
-        return self._client.post(
-            '/',
-            data={'username': username, 'password': password}
-        )
-
-    def logout(self):
-        return self._client.get('/auth/logout')
-
-@pytest.fixture
-def auth(client):
-    return AuthActions(client)
