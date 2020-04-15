3430992a389b7d845e727bceeb10f59fc8f629fa
alexanderdamiani <alexander.damiani15@gmail.com>
2020-04-15 15:23:50 -0400

    removed virtualenv

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy  # type: ignore
-from flask_bcrypt import Bcrypt  # type: ignore
+from flask_bcrypt import Bcrypt  # NOQA
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
